6eefb89443a47b062e3717fde2f466212a1ebcba
Vaishali IyerÃ <vaishali.k.iyer@gmail.com>
2021-11-16 14:59:52 +0100

    Datensatz Bayern und München

--- Bayern_MUC_daten.R
+++ Bayern_MUC_daten.R
@@ -1,32 +1,61 @@
 library(readr)
-divi_03_11<- read_csv("DIVI-Intensivregister-2021-11-03-12-15.csv.csv")
+divi_03_11<- read_csv("DIVI-Intensivregister-2021-11-03-12-15.csv")
 
 
+### Datensatz Bayern
 
 data_bayern <- subset(divi_03_11, bundesland == "09")
 
-## Bayern September
+
+### Datensatz München
+
+data_muenchen <- subset(data_bayern, gemeindeschluessel == "09162")
+
+
+
+
+
+
+
+## Datensatz Bayern für September
+
 data_bayern_sep <- subset(data_bayern, date >= "2020-09-01" & date <= "2020-09-30" )
 data_bayern$date <-as.Date(data_bayern$date, format = "%Y-%m-%d")
 
-## MUC in September
-data_bayern_sep_muc <- subset(data_bayern_sep, gemeindeschluessel == "09162")
 
-ggplot(data = data_bayern_sep_muc, aes(x = date, y = faelle_covid_aktuell)) +
+
+## Muenchen in September
+
+data_muc_sep <- subset(data_bayern_sep, gemeindeschluessel == "09162")
+
+
+
+### Verschiede GGplots zu Munchen in September
+
+library(ggplot2)
+
+ggplot(data = data_muc_sep_, aes(x = date, y = faelle_covid_aktuell)) +
   geom_bar(stat="identity")
 
-ggplot(data = data_bayern_sep, aes(x = date, y = faelle_covid_aktuell)) +
+ggplot(data = data_muc_sep, aes(x = date, y = faelle_covid_aktuell)) +
   geom_bar(stat="identity")
 
-ggplot(data = data_bayern_sep_muc, aes(x = date, y = faelle_covid_aktuell_invasiv_beatmet)) +
+ggplot(data = data_muc_sep, aes(x = date, y = faelle_covid_aktuell_invasiv_beatmet)) +
   geom_bar(stat="identity")
 
 
-## Bayern und MUC in Oktober
+
+
+
+## Bayern und München in Oktober
+
 data_bayern_oct <- subset(data_bayern, date >= "2020-10-01" & date <= "2020-10-31" )
-data_bayern_oct_muc <- subset(data_bayern_oct, gemeindeschluessel == "09162")
+data_muc_oct <- subset(data_bayern_oct, gemeindeschluessel == "09162")
 
-## Bayern Fälle
+
+
+
+## GGplot:  Bayern insgesamt Corona Fälle und Intensiv Beatmet
 
 ggplot(data = data_bayern, aes(x = date, y = faelle_covid_aktuell)) +
   geom_bar(stat="identity")
