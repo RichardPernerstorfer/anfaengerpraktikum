cd9255bb93f86b9863f33a138324e10eb1e57d1e
RichardPernerstorfer <85345323+RichardPernerstorfer@users.noreply.github.com>
2021-11-19 12:04:54 +0100

    Update Datensaetze_erstellen.R

--- Datensaetze_erstellen.R
+++ Datensaetze_erstellen.R
@@ -28,7 +28,7 @@ data_muenchen <- subset(data_bayern, gemeindeschluessel == "09162")
 data_IAA_germany <- subset(divi_17_11, date >= "2021-08-07" & date <= "2021-11-12" )
 data_IAA_bayern <- subset(data_bayern, date >= "2021-08-07" & date <= "2021-11-12" )
 data_IAA_muenchen <- subset(data_muenchen, date >= "2021-08-07" & date <= "2021-11-12" )
-
+data_IAA_bayern_ohne_muenchen  <- subset(data_bayern_ohne_muenchen, date >= "2021-08-07" & date <= "2021-11-12" )
 
 ### Datensatz Corona Demonstration
 
