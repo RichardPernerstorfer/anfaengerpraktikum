91da45610a50cb3bac94010a6b6f1f2d7e7030e1
Vaishali IyerÃ <vaishali.k.iyer@gmail.com>
2021-11-22 15:41:13 +0100

    Corona Demo Graphiken (noch nicht fertig)

---
+++ CoronaDemo_Graphiken.R
@@ -0,0 +1,91 @@
+### Corona Demonstration Graphiken
+
+library(ggplot2)
+library(lubridate)
+
+
+
+### Bar-Plots zu Corona Faellen in München von August bis November
+
+plot_muc_aug_cov <- ggplot(data = data_muc_aug, aes(x = date, y = faelle_covid_aktuell)) +
+  ggtitle("Corona Fälle in Muenchen in August") +
+  xlab("Datum") + ylab("Aktuelle Covid Fälle") + geom_bar(stat="identity")
+  
+plot_muc_sep_cov <- ggplot(data = data_muc_sep, aes(x = date, y = faelle_covid_aktuell)) +
+  ggtitle("Corona Fälle in Muenchen in September") +
+  xlab("Datum") + ylab("Aktuelle Covid Fälle") + geom_bar(stat="identity")
+
+plot_muc_oct_cov <- ggplot(data = data_muc_oct, aes(x = date, y = faelle_covid_aktuell)) +
+  ggtitle("Corona Fälle in Muenchen in Oktober") +
+  xlab("Datum") + ylab("Aktuelle Covid Fälle") + geom_bar(stat="identity")
+
+plot_muc_nov_cov <- ggplot(data = data_muc_nov, aes(x = date, y = faelle_covid_aktuell)) +
+  ggtitle("Corona Fälle in Muenchen in November") +
+  xlab("Datum") + ylab("Aktuelle Covid Fälle") + geom_bar(stat="identity")
+
+
+### Barplots zu Invasiv Beatmeten in Muenchen von August bis November
+
+plot_muc_aug_invb <-ggplot(data = data_muc_aug, aes(x = date, y = faelle_covid_aktuell_invasiv_beatmet)) +
+  ggtitle("Invasiv Beatmete Covid Fälle in München in August")+
+  xlab("Datum") + ylab("Aktuelle Covid invasiv beatmeten Fällen") +
+  geom_bar(stat="identity")
+
+plot_muc_sep_invb <-ggplot(data = data_muc_sep, aes(x = date, y = faelle_covid_aktuell_invasiv_beatmet)) +
+  ggtitle("Invasiv Beatmete Covid Fälle in München in September")+
+  xlab("Datum") + ylab("Aktuelle Covid invasiv beatmeten Fällen") +
+  geom_bar(stat="identity")
+
+plot_muc_oct_invb <-ggplot(data = data_muc_oct, aes(x = date, y = faelle_covid_aktuell_invasiv_beatmet)) +
+  ggtitle("Invasiv Beatmete Covid Fälle in München in Oktober")+
+  xlab("Datum") + ylab("Aktuelle Covid invasiv beatmeten Fällen") +
+  geom_bar(stat="identity")
+
+plot_muc_nov_invb <- ggplot(data = data_muc_nov, aes(x = date, y = faelle_covid_aktuell_invasiv_beatmet)) +
+  ggtitle("Invasiv Beatmete Covid Fälle in München in November")+
+  xlab("Datum") + ylab("Aktuelle Covid invasiv beatmeten Fällen") +
+  geom_bar(stat="identity")
+
+### Alle Bar-Plots zu Corona Faellen in München von August bis November in Einem
+
+install.packages("ggpubr")
+library(ggpubr)
+
+ggarrange(plot_muc_aug_cov, plot_muc_sep_cov, plot_muc_oct_cov, plot_muc_nov_cov ,
+          ncol = 2, nrow = 2)
+
+
+
+### Barplot Covid Faelle in Muenchen 
+
+
+ggplot(data = data_muc_aug_bis_nov, aes(x = date, y = faelle_covid_aktuell)) +
+  ggtitle("Corona Fälle in Muenchen von August bis November") +
+  xlab("Datum") + ylab("Aktuelle Covid Fälle") + geom_bar(stat="identity")
+
+
+### Gemeinsamer Bar-Plot Muc in August
+
+
+ggplot(data = data_muc_aug, aes(x=date)) +
+geom_bar(aes(y=faelle_covid_aktuell), stat = "identity", position = "identity", fill='lightblue', color = 'lightblue4')+
+geom_bar(aes(y=faelle_covid_aktuell_invasiv_beatmet), stat = "identity", position = "identity", fill = 'pink', color = 'red' )
+
+
+
+
+#### Ganz September Muenchen: Vergleich zwischen aktuele covid-faelle und Intensiv beatmete Faelle
+
+color_code <- c("faelle_covid_aktuell" = "blue",
+                "faelle_covid_aktuell_invasiv_beatmet" = "red")
+
+ggplot(data = data_muc_sep)+ aes(x = date) + geom_line(aes( y = faelle_covid_aktuell, col = "faelle_covid_aktuell")) +
+  geom_line(aes(y = faelle_covid_aktuell_invasiv_beatmet, col = "faelle_covid_aktuell_invasiv_beatmet" ))
+
+scale_color_manual("Index", values = color_code)
+
+
+
+### 
+
+
--- Corona_Demo_Event.R
+++ Corona_Demo_Event.R
@@ -1,37 +1,18 @@
 #### Corona Demonstration in Muenchen am 12.09.2020
 
-library(ggplot2)
-
-ggplot(data = data_muc_sep, aes(x = date, y = faelle_covid_aktuell)) +
-  geom_bar(stat="identity")
-
-ggplot(data = data_muc_sep, aes(x = date, y = faelle_covid_aktuell_invasiv_beatmet)) +
-  geom_bar(stat="identity")
-
-
-#### Ganz September Muenchen: Vergleich zwischen aktuele covid-faelle und Intensiv beatmete Faelle
-
-color_code <- c("faelle_covid_aktuell" = "blue",
-                "faelle_covid_aktuell_invasiv_beatmet" = "red")
-
-ggplot(data = data_muc_sep)+ aes(x = date)+ geom_line(aes( y = faelle_covid_aktuell, col = "faelle_covid_aktuell")) +
-  geom_line(aes(y=faelle_covid_aktuell_invasiv_beatmet, col= "faelle_covid_aktuell_invasiv_beatmet" ))
-
-scale_color_manual("Index", values = color_code)
-
 
 ### Mittelwert Covid Faelle in Muenchen
 
-mean_muc_aug_covfaelle <- mean(data_muc_aug$faelle_covid_aktuell)
+mean_muc_aug_covfaelle <- mean(data_muc_aug$faelle_covid_aktuell)  ## 9.1
 mean_muc_aug_covfaelle
 
-mean_muc_sep_covfaelle <- mean(data_muc_sep$faelle_covid_aktuell)
+mean_muc_sep_covfaelle <- mean(data_muc_sep$faelle_covid_aktuell)  ## 12.1
 mean_muc_sep_covfaelle
 
-mean_muc_oct_covfaelle <- mean(data_muc_oct$faelle_covid_aktuell)
+mean_muc_oct_covfaelle <- mean(data_muc_oct$faelle_covid_aktuell)  ## 15.4
 mean_muc_oct_covfaelle
 
-mean_muc_nov_covfaelle <- mean(data_muc_nov$faelle_covid_aktuell)
+mean_muc_nov_covfaelle <- mean(data_muc_nov$faelle_covid_aktuell)  ## 81
 mean_muc_nov_covfaelle
 
 
@@ -53,14 +34,26 @@ mean_muc_nov_invbeatmet <- mean(data_muc_nov$faelle_covid_aktuell_invasiv_beatme
 mean_muc_nov_invbeatmet
 
 
+
+
 ### Korrelation zwischen Covid Fälle und Intensiv Beatmet in Muenchen September
+
+
 cor(data_muc_sep$faelle_covid_aktuell , data_muc_sep$faelle_covid_aktuell_invasiv_beatmet)
+
+## Einseitigen Test
 cor.test(data_muc_sep$faelle_covid_aktuell , data_muc_sep$faelle_covid_aktuell_invasiv_beatmet)
 
 ##Positiven Zsmhang berechnen (einseitigen Test)
 cor.test(data_muc_sep$faelle_covid_aktuell , data_muc_sep$faelle_covid_aktuell_invasiv_beatmet, alternative = "greater")
 
 ### A:  starker Zusammenhang !!!
+
+
+
+
+
+### Korrelation zwischen Covid Fälle und Intensiv Beatmet in Muenchen October
 
 
 
