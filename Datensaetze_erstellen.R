ac255a277375da24ded8fa5baeb690bedd411f22
RichardPernerstorfer <85345323+RichardPernerstorfer@users.noreply.github.com>
2021-11-27 16:57:22 +0100

    Update Datensaetze_erstellen.R

--- Datensaetze_erstellen.R
+++ Datensaetze_erstellen.R
@@ -38,7 +38,8 @@ data_IAA_germany <- subset(divi_17_11, date >= "2021-08-07" & date <= "2021-11-1
 data_IAA_bayern <- subset(data_bayern, date >= "2021-08-07" & date <= "2021-11-12" )
 data_IAA_muenchen <- subset(data_muenchen, date >= "2021-08-07" & date <= "2021-11-12" )
 data_IAA_bayern_ohne_muenchen  <- subset(data_bayern_ohne_muenchen, date >= "2021-08-07" & date <= "2021-11-12" )
-
+data_IAA_anteil_muenchen_bayern$anteil <- data_IAA_muenchen$faelle_covid_aktuell/ (aggregate(data_IAA_bayern$faelle_covid_aktuell, data_IAA_bayern[1], sum))[2]
+data_IAA_anteil_muenchen_bayern$date <- data_IAA_muenchen$date
 
 ### Datensatz Corona Demonstration
 
