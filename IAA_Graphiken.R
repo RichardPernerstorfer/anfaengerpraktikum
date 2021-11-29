ac92c13cf4e32268d3eee54182bdc04ac15b133a
RichardPernerstorfer <85345323+RichardPernerstorfer@users.noreply.github.com>
2021-11-29 18:29:38 +0100

    Update IAA_Graphiken.R

--- IAA_Graphiken.R
+++ IAA_Graphiken.R
@@ -1,34 +1,46 @@
 library(ggplot2)
 library(lubridate)
 ### covid fälle um die IAA
+
 # Fälle IAA Deutschland
 ggplot(data = data_IAA_germany, aes(x = date, y = faelle_covid_aktuell)) +
   geom_bar(stat="identity") + 
   labs(x = "Datum", y = "aktuelle Covid-Fälle", title = "Aktuelle Covid-Fälle um die IAA deutschlandweit") + 
   annotate("rect", fill = "pink", alpha = 0.4, 
            xmin = as.Date(c("2021-09-07")), xmax = as.Date(c("2021-09-12")),
-           ymin = 0, ymax = Inf)
+           ymin = 0, ymax = Inf) +
+  scale_x_date(date_breaks = "1 week") + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
+
 # Fälle IAA Bayern
 ggplot(data = data_IAA_bayern, aes(x = date, y = faelle_covid_aktuell)) +
   geom_bar(stat="identity") + 
   labs(x = "Datum", y = "aktuelle Covid-Fälle", title = "Aktuelle Covid-Fälle um die IAA bayernweit") +
   annotate("rect", fill = "pink", alpha = 0.4, 
            xmin = as.Date(c("2021-09-07")), xmax = as.Date(c("2021-09-12")),
-           ymin = 0, ymax = Inf)
+           ymin = 0, ymax = Inf)  +
+  scale_x_date(date_breaks = "1 week") + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
+
 # Fälle IAA München
 ggplot(data = data_IAA_muenchen, aes(x = date, y = faelle_covid_aktuell)) +
   geom_bar(stat="identity") + 
   labs(x = "Datum", y = "aktuelle Covid-Fälle", title = "Aktuelle Covid-Fälle um die IAA in München") +
   annotate("rect", fill = "pink", alpha = 0.4, 
            xmin = as.Date(c("2021-09-07")), xmax = as.Date(c("2021-09-12")),
-           ymin = 0, ymax = Inf)
+           ymin = 0, ymax = Inf)  +
+  scale_x_date(date_breaks = "1 week") + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
+
 # Fälle IAA Bayern ohne München
 ggplot(data = data_IAA_bayern_ohne_muenchen, aes(x = date, y = faelle_covid_aktuell)) +
   geom_bar(stat="identity") + 
   labs(x = "Datum", y = "aktuelle Covid-Fälle", title = "Aktuelle Covid-Fälle um die IAA in Bayern ohne München") +
   annotate("rect", fill = "pink", alpha = 0.4, 
            xmin = as.Date(c("2021-09-07")), xmax = as.Date(c("2021-09-12")),
-           ymin = 0, ymax = Inf)
+           ymin = 0, ymax = Inf)  +
+  scale_x_date(date_breaks = "1 week") + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
 
 # Fälle IAA Anteil München an Bayern
 ggplot(data = data_IAA_anteil_muenchen_bayern, aes(y = x, x = date)) + 
@@ -36,7 +48,10 @@ ggplot(data = data_IAA_anteil_muenchen_bayern, aes(y = x, x = date)) +
   labs(x = "Datum", y = "Anteil aktueller Covid-Fälle", title = "Anteil der bayerischen Covid-Fälle von München") +
   annotate("rect", fill = "pink", alpha = 0.4, 
            xmin = as.Date(c("2021-09-07")), xmax = as.Date(c("2021-09-12")),
-           ymin = 0, ymax = Inf)
+           ymin = 0, ymax = Inf)  +
+  scale_x_date(date_breaks = "1 week") + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
+
 
 # Belegte Intensivbetten IAA
 ggplot() + 
@@ -45,13 +60,18 @@ ggplot() +
   labs(x = "Datum", y = "Intensivbetten absolut ", title = "Intensivbetten in München") +
   annotate("rect", fill = "pink", alpha = 0.4, 
            xmin = as.Date(c("2021-09-07")), xmax = as.Date(c("2021-09-12")),
-           ymin = 0, ymax = Inf)
+           ymin = 0, ymax = Inf)  +
+  scale_x_date(date_breaks = "1 week") + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
+
 # belegte Intensivbetten Prozent
 ggplot(data = data_IAA_muenchen, aes(x = date, y = betten_anteil)) +
   geom_line(stat="identity") + 
   labs(x = "Datum", y = "Anteil belegter Intensivbetten", title = "Anteil belegter Intensivbetten in München um IAA") +
   annotate("rect", fill = "pink", alpha = 0.4, 
            xmin = as.Date(c("2021-09-07")), xmax = as.Date(c("2021-09-12")),
-           ymin = 0, ymax = Inf)
+           ymin = 0, ymax = Inf)  +
+  scale_x_date(date_breaks = "1 week") + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
 
   
