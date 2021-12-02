55460d89211b8a4758f787d2e0df47ee9c52e55b
RichardPernerstorfer <85345323+RichardPernerstorfer@users.noreply.github.com>
2021-12-02 22:21:32 +0100

    Add files via upload

---
+++ Beispielplot.R
@@ -0,0 +1,14 @@
+library(ggplot2)
+Fälle_Hospitalisierung_Alter$A5_bis_A15[13] <- 0
+ggplot() + 
+  geom_line(aes(x = date, y = A0_bis_A4, group = 1, color = "Altergruppe 0 bis 4"), size = 2, data = Fälle_Hospitalisierung_Alter) +
+  geom_line(aes(x = date, y = A5_bis_A15, group = 1, color = "Altergruppe 5 bis 15"), size = 2, data = Fälle_Hospitalisierung_Alter) +
+  geom_line(aes(x = date, y = A15_bis_A34, group = 1, color = "Altergruppe 15 bis 34" ), size = 2, data = Fälle_Hospitalisierung_Alter) +
+  geom_line(aes(x = date, y = A35_bis_A59, group = 1, color = "Altergruppe 35 bis 59"), size = 2, data = Fälle_Hospitalisierung_Alter) +
+  geom_line(aes(x = date, y = A60_bis_A79, group = 1, color = "Altergruppe 60 bis 79"), size = 2, data = Fälle_Hospitalisierung_Alter) +
+  geom_line(aes(x = date, y = A80_plus, group = 1, color = "Altergruppe 80 +"), size = 2, data = Fälle_Hospitalisierung_Alter) +
+  theme(axis.text.x = element_text(size = 12, face = "bold")) + 
+  theme(axis.text.y = element_text(size = 12)) +
+  labs(x = "Datum in Kalenderwochen", y = "Covid-Fälle", title = "Covid-Fälle pro Altersgruppe in Deutschland", color = "Altersgruppen") +
+  theme(text = element_text(size = 40)) +
+  scale_x_discrete(guide = guide_axis(check.overlap = TRUE))
