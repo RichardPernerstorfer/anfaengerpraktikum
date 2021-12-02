f561e1ef42b118b9e9979e2471a1b6242e583ca3
vaishali0810 <vaishali.k.iyer@gmail.com>
2021-12-02 19:43:09 +0100

    Delete vaccination_timeseries.R

--- vaccination_timeseries.R
+++
@@ -1,8 +0,0 @@
-## Impfstoff Data
-
-vaccination_timeseries <- read.table(file='germany_vaccinations_timeseries_v2.tsv')
-
-
-## Spaltennamen Änderung
-colnames(vaccination_timeseries) <- as.character(vaccination_timeseries[1,])
-vaccination_timeseries <- vaccination_timeseries[-1, ]
