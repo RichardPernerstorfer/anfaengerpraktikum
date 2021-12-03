dde58c49afeafc0aa223fb876198a06f4092ec9b
Aylin Seibold <ayla-99@live.de>
2021-12-03 12:25:58 +0100

    update

--- Datensaetze_erstellen.R
+++ Datensaetze_erstellen.R
@@ -65,9 +65,13 @@ data_CorDemo_bayern_ohne_muenchen <- subset(data_bayern_ohne_muenchen, date >= "
 ### Datensatz Black Lives Matter Demonstration 
 
 data_BLM_germany <- subset(divi_17_11, date >= "2020-05-06" & date <= "2020-08-06" )
-data_BLM_bayern <- subset(data_bayern, date >= "2020-05-11" & date <= "2020-08-06" )
+data_BLM_bayern <- subset(data_bayern, date >= "2020-05-06" & date <= "2020-08-06" )
 data_BLM_muenchen <- subset(data_muenchen, date >= "2020-05-06" & date <= "2020-08-06" )
 data_BLM_bayern_ohne_muenchen  <- subset(data_bayern_ohne_muenchen, date >= "2020-05-06" & date <= "2020-05-06" )
+
+
+### Anteil München an Bayern
+data_BLM_muenchen$anteil <- data_BLM_muenchen$faelle_covid_aktuell / as.vector(unlist(aggregate(data_BLM_bayern$faelle_covid_aktuell, data_BLM_bayern[1], sum)[2]))
 
 
 ## Gelsenkirchen
