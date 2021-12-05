433358a5e8cdf4d804981f4f2c8d76ccf2090095
XIAO FU <85993523+tianlianglefx@users.noreply.github.com>
2021-12-05 22:03:26 +0100

    Update impfung plot.R

--- impfung plot.R
+++ impfung plot.R
@@ -52,7 +52,40 @@ plot_impfquote<-ggplot()+
   theme(text = element_text(size=8),axis.text.x=element_text(angle=50, hjust=1),legend.position = c( .1, .8))
 plot_impfquote
 
-###3 Graphen zusammen
+##3 Graphen zusammen
 library(ggpubr)
 ggarrange(plot_deutschland_covid_faelle_vor_impfung,plot_deutschland_covid_faelle_nach_impfung, plot_impfquote,
+          ncol = 1, nrow = 3)
+
+
+###Plot Hospitalisierungsinzidenz Bayern vs Impfquote Deutschlands
+#Subset Hosp_data_bayern vor/nach der Impfung
+Hosp_data_bayern_vor_impfung<-subset(Hosp_data_bayern,Meldedatum<="2020-12-27")
+View(Hosp_data_bayern_vor_impfung)
+Hosp_data_bayern_nach_impfung<-subset(Hosp_data_bayern,Meldedatum>="2020-12-27")
+View(Hosp_data_bayern_nach_impfung)
+
+#Plot Hosp_data_bayern vor der Impfung
+plot_Hosp_data_bayern_vor_impfung<-ggplot(data =Hosp_data_bayern_vor_impfung)+
+  geom_bar(aes(x=Meldedatum,y=Hospitalisierung/130.7672),stat="identity")+
+  scale_x_date(date_labels = "%b %d",date_breaks = "4 week")+
+  theme(text = element_text(size=8),axis.text.x=element_text(angle=50, hjust=1))+
+  labs(x = "Datum", y = "Hospitalisierungsinzidenz", title = "Hospitalisierungsinzidenz in Bayern vor der Impfung")+
+  geom_vline(xintercept= as.Date(c("2020-12-27")), color = "red", size = 1)+
+  scale_y_continuous(breaks=c(0,1,2))
+plot_Hosp_data_bayern_vor_impfung
+
+#Plot Hosp_data_bayern nach der Impfung
+plot_Hosp_data_bayern_nach_impfung<-ggplot(data =Hosp_data_bayern_nach_impfung)+
+  geom_bar(aes(x=Meldedatum,y=Hospitalisierung/130.7672),stat="identity")+
+  scale_x_date(date_labels = "%b %d",date_breaks = "4 week")+
+  theme(text = element_text(size=8),axis.text.x=element_text(angle=50, hjust=1))+
+  labs(x = "Datum", y = "Hospitalisierungsinzidenz", title = "Hospitalisierungsinzidenz in Bayern nach der Impfung")+
+  geom_vline(xintercept= as.Date(c("2020-12-27")), color = "red", size = 1)+
+  scale_y_continuous(breaks=c(0,1,2))
+plot_Hosp_data_bayern_nach_impfung
+
+##3 Graphen zusammen
+library(ggpubr)
+ggarrange(plot_Hosp_data_bayern_vor_impfung,plot_Hosp_data_bayern_nach_impfung, plot_impfquote,
           ncol = 1, nrow = 3)
