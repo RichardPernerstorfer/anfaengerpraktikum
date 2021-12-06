17ad7b98b9c754627266fc669c4402bbed1d11a0
RichardPernerstorfer <85345323+RichardPernerstorfer@users.noreply.github.com>
2021-12-06 16:12:12 +0100

    Update Allgemein.R

--- Allgemein.R
+++ Allgemein.R
@@ -1,30 +1,4 @@
-#Allgemein
 
-
-## Impfstoff Data
-
-vaccination_timeseries <- read.table(file='germany_vaccinations_timeseries_v2.tsv')
-
-## Spaltennamen Änderung
-colnames(vaccination_timeseries) <- as.character(vaccination_timeseries[1,])
-vaccination_timeseries <- vaccination_timeseries[-1, ]
-
-## Formatierung Date
-myformat2<-"%Y-%m-%d"
-vaccination_timeseries$date<-as.Date(vaccination_timeseries$date,myformat2)
-View(vaccination_timeseries)
-
-
-
-## RKI Datensatz Bayern
-
-install.packages("vroom")
-library(vroom)
-
-rki_bayern_data <- vroom('RKI_COVID19_Bayern.zip')
-
-## Nach Datum sortieren
-rki_bayern_data[order(as.Date(rki_bayern_data$Meldedatum, format="%Y/%m/%d")),]
 
 
                       
