45caeb303cb1dce694a1da9ac3c7fea81a0fa02a
RichardPernerstorfer <85345323+RichardPernerstorfer@users.noreply.github.com>
2021-12-06 17:50:52 +0100

    Update IAA_Graphiken.R

--- IAA_Graphiken.R
+++ IAA_Graphiken.R
@@ -11,7 +11,7 @@ ggplot(data = data_IAA_germany, aes(x = date, y = faelle_covid_aktuell)) +
            xmin = as.Date(c("2021-09-07")), xmax = as.Date(c("2021-09-12")),
            ymin = 0, ymax = Inf) +
   scale_x_date(date_breaks = "1 week") + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
 
 # Fälle IAA Bayern
 IAA_bayern_plot <- ggplot(data = data_IAA_bayern, aes(x = date, y = faelle_covid_aktuell)) +
@@ -56,7 +56,9 @@ ggplot(data = data_IAA_muenchen, aes(y = anteil, x = date)) +
            xmin = as.Date(c("2021-09-07")), xmax = as.Date(c("2021-09-12")),
            ymin = 0, ymax = Inf)  +
   scale_x_date(date_breaks = "1 week") + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
+  theme(axis.text.x = element_text(size = 18, angle = 45, vjust = 1, hjust = 1, face = "bold")) +
+  theme(axis.text.y = element_text(size = 18, face = "bold")) +
+ theme(text = element_text(size = 30))
 
 
 # Belegte Intensivbetten IAA
