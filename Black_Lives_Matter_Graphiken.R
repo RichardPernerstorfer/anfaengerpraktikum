27118c61e20dab4b29e82f442ead7d211881fab1
aylin-seibold <85242452+aylin-seibold@users.noreply.github.com>
2021-12-07 19:35:30 +0100

    Update Black_Lives_Matter_Graphiken.R

--- Black_Lives_Matter_Graphiken.R
+++ Black_Lives_Matter_Graphiken.R
@@ -5,22 +5,28 @@ library(patchwork)
 ### Absolute Covid-Fälle Bayern
 
 plot_BLM_bayern <- ggplot(data = data_BLM_bayern, aes(x = date, y = faelle_covid_aktuell)) +
-  ggtitle("Bayern") +
-  xlab("Datum") + ylab("Aktuelle Covid-Fälle") + geom_bar(stat="identity") +
-  geom_vline(xintercept= as.Date(c("2020-06-06")), color = "red", size = 1) +
-  geom_vline(xintercept= as.Date(c("2020-06-13")), color = "blue", size = 1) +
-  scale_x_date(date_breaks = "2 week", date_labels = "%b %d")
+  labs(x = "Datum", y = "Aktuelle Covid-Fälle",title = "Aktuelle Covid-Fälle in Bayern") +
+  geom_bar(stat="identity") +
+  geom_vline(xintercept= as.Date(c("2020-06-06")), color = "red", size = 2) +
+  geom_vline(xintercept= as.Date(c("2020-06-13")), color = "blue", size = 2) +
+  scale_x_date(date_breaks = "1 week") +
+  theme(axis.text.x = element_text(size = 18, angle = 45, vjust = 1, hjust = 1, face = "bold")) +
+  theme(axis.text.y = element_text(size = 18, face = "bold")) +
+  theme(text = element_text(size = 30))
 plot_BLM_bayern
 
 
 ### Absolute Covid-Fälle München
 
 plot_BLM_muenchen <- ggplot(data = data_BLM_muenchen, aes(x = date, y = faelle_covid_aktuell)) +
-  ggtitle("München") +
-  xlab("Datum") + ylab("Aktuelle Covid-Fälle") + geom_bar(stat="identity") + 
-  geom_vline(xintercept= as.Date(c("2020-06-06")), color = "red", size = 1) +
-  geom_vline(xintercept= as.Date(c("2020-06-13")), color = "blue", size = 1) +
-  scale_x_date(date_breaks = "2 week", date_labels = "%b %d")
+  labs(x = "Datum", y = "Aktuelle Covid-Fälle",
+       title = "Aktuelle Covid-Fälle in München") + geom_bar(stat="identity") + 
+  geom_vline(xintercept= as.Date(c("2020-06-06")), color = "red", size = 2) +
+  geom_vline(xintercept= as.Date(c("2020-06-13")), color = "blue", size = 2) +
+  scale_x_date(date_breaks = "1 week") +
+  theme(axis.text.x = element_text(size = 18, angle = 45, vjust = 1, hjust = 1, face = "bold")) +
+  theme(axis.text.y = element_text(size = 18, face = "bold")) +
+  theme(text = element_text(size = 30))
 plot_BLM_muenchen
 
 
@@ -28,51 +34,49 @@ plot_BLM_muenchen
 
 plot_muenchen_bayern <- plot_BLM_muenchen / plot_BLM_bayern
 
-plot_muenchen_bayern + plot_annotation(
-  title = "Event 1: Black Lives Matter Demonstration
-  Aktuelle Covid-Fälle zwischen Mai und August 2020", 
-  subtitle = "Bestätigte Covid-19 Fälle in München und Bayern um die Black Lives Matter Demonstration"
-)
+plot_muenchen_bayern <- plot_muenchen_bayern + 
+                        plot_annotation(theme = theme(plot.title = element_text(size = 30), plot.subtitle = element_text(size = 20)),
+                                        
+                                        title = "Event 1: Black Lives Matter Demonstration", 
+                        subtitle = "Anzahl bestätigter Covid-Fälle in München und Bayern um die Black Lives Matter Demonstration 2020")
+                        
 
 plot_muenchen_bayern
 
 
-### Anteil Covid-Fälle München an Bayern
+### Anteil Covid-Fälle München an Bayern 
 
 plot_anteil_muenchen_bayern <- ggplot( data = data_BLM_muenchen, aes(x = date, y = anteil)) +
-  geom_line() + 
-  plot_annotation(title = "Event 1: Black Lives Matter Demonstration",
-                  subtitle = "Anteil Covid-Fälle Münchens an Covid-Fälle Bayerns zwischen Mai und August 2020" ) + xlab("Datum") + ylab("Anteil Covid-Fälle") +
-  geom_vline(xintercept= as.Date(c("2020-06-06")), color = "red", size = 1) +
-  geom_vline(xintercept= as.Date(c("2020-06-13")), color = "blue", size = 1) +
-  scale_x_date(date_breaks = "2 week", date_labels = "%b %d") +
-  expand_limits(y = 0)
+  geom_line(size = 2) + 
+  labs(x = "Datum", y = "Anteil Covid-Fälle",
+       title = "Event 1: Black Lives Matter Demonstration",
+       subtitle = "Anteil Covid-Fälle Münchens an Covid-Fälle Bayerns um die Black Lives Matter Demonstration 2020") +
+  geom_vline(xintercept= as.Date(c("2020-06-06")), color = "red", size = 2) +
+  geom_vline(xintercept= as.Date(c("2020-06-13")), color = "blue", size = 2) +
+  scale_x_date(date_breaks = "1 week") +
+  expand_limits(y = 0) +
+  theme(axis.text.x = element_text(size = 18, angle = 45, vjust = 1, hjust = 1, face = "bold")) +
+  theme(axis.text.y = element_text(size = 18, face = "bold")) +
+  theme(text = element_text(size = 30))
 plot_anteil_muenchen_bayern
 
 
-### Anteil Betten Muc an Bayern
+### Anteil Betten München an Bayern
 
 plot_anteil_betten_muc_bay <- ggplot( data = data_BLM_muenchen, aes(x = date, y = anteil_betten)) +
-  geom_line() +
-  ggtitle("Anteil der belegten Betten Münchens in gesamt Bayern") + xlab("Datum") + ylab("Anteil belegter Betten") + 
-  geom_vline(xintercept= as.Date(c("2020-06-06")), color = "red", size = 1) +
-  geom_vline(xintercept= as.Date(c("2020-06-13")), color = "violet", size = 1) +
-  scale_x_date(date_breaks = "2 week", date_labels = "%b %d") +
-  expand_limits(y = 0)
-plot_anteil_betten_muc_bay
-
-
-### Bettenbelegung Bayern
-
-plot_betten_bayern <- ggplot() + 
-  geom_bar(aes(y = betten_belegt + betten_frei, x = date), data = data_BLM_bayern, stat = "identity", fill = "blue") + 
-  geom_bar(aes(y = betten_belegt, x = date), data = data_BLM_bayern, stat = "identity") + 
-  labs(x = "Datum", y = "Intensivbetten absolut ", title = "Intensivbetten in Bayern") +
+  geom_line(size = 2) +
+  labs(x = "Datum", y = "Anteil belegter Intensivbetten ",
+  title = "Event 1: Black Lives Matter Demonstration", 
+  subtitle = "Anteil belegter Intensivbetten Münchens an belegter Intensivbetten Bayerns 
+  um die Black Lives Matter Demonstration 2020") + 
   geom_vline(xintercept= as.Date(c("2020-06-06")), color = "red", size = 2) +
-  geom_vline(xintercept= as.Date(c("2020-06-13")), color = "violet", size = 2) +
-  scale_x_date(date_breaks = "2 week", date_labels = "%b %d")
-
-plot_betten_bayern
+  geom_vline(xintercept= as.Date(c("2020-06-20")), color = "violet", size = 2) +
+  scale_x_date(date_breaks = "1 week") +
+  expand_limits(y = 0) +
+  theme(axis.text.x = element_text(size = 18, angle = 45, vjust = 1, hjust = 1, face = "bold")) +
+  theme(axis.text.y = element_text(size = 18, face = "bold")) +
+  theme(text = element_text(size = 30))
+plot_anteil_betten_muc_bay
 
 
 ### Belegte Betten München
@@ -80,11 +84,15 @@ plot_betten_bayern
 plot_betten_muenchen <- ggplot(data = data_BLM_muenchen) + 
   geom_bar(aes(y = betten_belegt + betten_frei, x = date), data = data_BLM_muenchen, stat = "identity", fill = "blue") + 
   geom_bar(aes(x = date, y = betten_belegt), stat = "identity") +
-  ggtitle("Belegte Betten zwischen Mai und August 2020 in München ") +
-  xlab("Datum") + ylab("Belegte Betten") + 
+  labs(x = "Datum", y = "Intensivbetten absolut",title = "Event 1: Black Lives Matter Demonstration",
+       subtitle = "Anzahl aller vohandenden Intensivbetten und Anzahl belegter Intensivbetten in München 
+       um die Black Lives Matter Demonstration 2020") + 
   geom_vline(xintercept= as.Date(c("2020-06-06")), color = "red", size = 2) +
-  geom_vline(xintercept= as.Date(c("2020-06-13")), color = "violet", size = 2) +
-  scale_x_date(date_breaks = "2 week", date_labels = "%b %d")
+  geom_vline(xintercept= as.Date(c("2020-06-20")), color = "violet", size = 2) +
+  scale_x_date(date_breaks = "1 week") +
+  theme(axis.text.x = element_text(size = 18, angle = 45, vjust = 1, hjust = 1, face = "bold")) +
+  theme(axis.text.y = element_text(size = 18, face = "bold")) +
+  theme(text = element_text(size = 30))
 plot_betten_muenchen
 
 
@@ -131,16 +139,17 @@ plot_rel_betten_bayern <- ggplot(data = data_BLM_bayern, aes(x = date, y = bette
   geom_vline(xintercept= as.Date(c("2020-06-06")), color = "red", size = 1)
 plot_rel_betten_bayern
 
+### Bettenbelegung Bayern Anhang!
 
+plot_betten_bayern <- ggplot() + 
+  geom_bar(aes(y = betten_belegt + betten_frei, x = date), data = data_BLM_bayern, stat = "identity", fill = "blue") + 
+  geom_bar(aes(y = betten_belegt, x = date), data = data_BLM_bayern, stat = "identity") + 
+  labs(x = "Datum", y = "Intensivbetten absolut ", title = "Intensivbetten in Bayern") +
+  geom_vline(xintercept= as.Date(c("2020-06-06")), color = "red") +
+  geom_vline(xintercept= as.Date(c("2020-06-20")), color = "violet") +
+  scale_x_date(date_breaks = "1 week", date_labels = "%b %d") +
+  theme(axis.text.x = element_text(size = 18, angle = 45, vjust = 1, hjust = 1, face = "bold")) +
+  theme(axis.text.y = element_text(size = 18, face = "bold")) +
+  theme(text = element_text(size = 30))
 
-
-
-
-
-
-
-
-
-
-
-
+plot_betten_bayern
