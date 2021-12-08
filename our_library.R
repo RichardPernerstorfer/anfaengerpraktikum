f05f85bd7a9c7ad1fccda0eb5a6e2e62687d1f78
RichardPernerstorfer <85345323+RichardPernerstorfer@users.noreply.github.com>
2021-12-08 20:15:19 +0100

    Add files via upload

---
+++ our_library.R
@@ -0,0 +1,13 @@
+### Immer zuerst durchführen
+
+library(dplyr)
+library(ggplot2)
+library(patchwork)
+library(ggpubr)
+library(readr)
+library(vroom)
+library(rio)
+library(stringr)
+library(tidyr)
+library(tidyverse)
+library(lubridate)
