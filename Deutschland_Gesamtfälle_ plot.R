c95a6d0879612d8f958de0c23eeb20bb755d427c
RichardPernerstorfer <85345323+RichardPernerstorfer@users.noreply.github.com>
2021-12-09 20:22:48 +0100

    Update Deutschland_Gesamtfälle_ plot.R

--- Deutschland_Gesamtfälle_ plot.R
+++ Deutschland_Gesamtfälle_ plot.R
@@ -1,13 +1,13 @@
 # Deutschland Graphik
 
- ggplot(data = RKI_Infektionen, aes(x = Datum, y = (Infektionen / 83129285) *100000)) +
-  geom_bar(stat="identity", width = 1) + 
-  labs(x = "Datum", y = "Neu-Infektionen mit Covid-19 pro 100000 Einwohner", title = "Covid-Infektionen Gesamt in Deutschland") + 
-  scale_x_date(date_breaks = "1 month", date_labels = "%b %y") +
-  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) +
-  theme(axis.text.x = element_text(size = 18, angle = 45, vjust = 1, hjust = 1, face = "bold")) +
-  theme(axis.text.y = element_text(size = 18, face = "bold")) +
-  theme(text = element_text(size = 30)) 
+ ggplot(data = subset(RKI_Infektionen, Datum >= "2020-03-01"), aes(x = Datum, y = (Infektionen / 83129285) *100000)) +
+     geom_bar(stat="identity", width = 1) + 
+     labs(x = "Datum", y = "Neu-Infektionen mit Covid-19 pro 100.000 Einwohner", title = "Tägliche Covid-Infektionen pro 100.000 Einwohner in Deutschland") + 
+     scale_x_date(date_breaks = "1 month", date_labels = "%b %y") +
+     theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) +
+     theme(axis.text.x = element_text(size = 18, angle = 45, vjust = 1, hjust = 1, face = "bold")) +
+     theme(axis.text.y = element_text(size = 18, face = "bold")) +
+     theme(text = element_text(size = 30)) 
 
 
 
