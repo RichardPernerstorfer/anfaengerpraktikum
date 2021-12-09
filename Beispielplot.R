5878d0c2a3b300583bb8a7f3de52653f3b222913
RichardPernerstorfer <85345323+RichardPernerstorfer@users.noreply.github.com>
2021-12-09 14:54:39 +0100

    Update Beispielplot.R

--- Beispielplot.R
+++ Beispielplot.R
@@ -59,24 +59,24 @@ ggplot(data = data_bayern, aes(x = date, y = faelle_covid_aktuell)) +
   scale_x_date( date_breaks = "1 month") +
   theme(axis.text.x=element_text(angle=50, hjust=1))+
   geom_vline(xintercept= as.Date(c("2020-09-12")), color = "red2", size = 1) +
-  geom_text(aes(x=as.Date(c("2020-09-12")), label="Corona-Demonstration", y = 750), colour="blue", angle=90, vjust = 1.2, text = element_text(size=10))+
+  geom_text(aes(x=as.Date(c("2020-09-12")), label="Corona-Demonstration", y = 750), colour="blue", angle=90, vjust = 1.2, text = element_text(size=18))+
   
   geom_vline(xintercept= as.Date(c("2020-06-06")), color = "red", size = 1) +
-  geom_text(aes(x=as.Date(c("2020-06-06")), label="BLM-Demonstration", y = 750), colour="blue", angle=90, vjust = 1.2, text = element_text(size=10))+
+  geom_text(aes(x=as.Date(c("2020-06-06")), label="BLM-Demonstration", y = 750), colour="blue", angle=90, vjust = 1.2, text = element_text(size=18))+
   
   annotate("rect", fill = "red", alpha = 0.4, 
-           xmin = as.Date(c("2021-06-11")), xmax = as.Date(c("2021-07-11")),
+           xmin = as.Date(c("2021-06-11")) - 0.5, xmax = as.Date(c("2021-07-11")) + 0.5,
            ymin = 0, ymax = Inf) +
   geom_vline(xintercept= as.Date(c("2021-06-15")), color = "red", size = 1)+
   geom_vline(xintercept= as.Date(c("2021-06-19")), color = "red", size = 1)+
   geom_vline(xintercept= as.Date(c("2021-06-23")), color = "red", size = 1)+
   geom_vline(xintercept= as.Date(c("2021-07-02")), color = "red", size = 1)+
-  geom_text(aes(x=as.Date(c("2021-07-12")), label="Fußball-EM", y = 750), colour="blue", angle=90, vjust = 1.2, text = element_text(size=10))+
+  geom_text(aes(x=as.Date(c("2021-07-12")), label="Fußball-EM", y = 750), colour="blue", angle=90, vjust = 1.2, text = element_text(size=18))+
   
   annotate("rect", fill = "red", alpha = 0.4, 
            xmin = as.Date(c("2021-09-07")), xmax = as.Date(c("2021-09-12")),
            ymin = 0, ymax = Inf)  +
-  geom_text(aes(x=as.Date(c("2021-09-12")), label="IAA", y = 750), colour="blue", angle=90, vjust = 1.2, text = element_text(size=10))+
+  geom_text(aes(x=as.Date(c("2021-09-12")), label="IAA", y = 750), colour="blue", angle=90, vjust = 1.2, text = element_text(size=18))+
   
   theme(axis.text.x = element_text(size = 18, face = "bold")) + 
   theme(axis.text.y = element_text(size = 18)) +
