bd864dfe53b38c3c43bf289b2d96a2fb0e5f47be
RichardPernerstorfer <85345323+RichardPernerstorfer@users.noreply.github.com>
2021-12-13 19:40:44 +0100

    Update IAA_Graphiken.R

--- IAA_Graphiken.R
+++ IAA_Graphiken.R
@@ -13,65 +13,39 @@ divi_muenchen <- subset(divi_IAA, gemeindeschluessel == "09162")
 divi_muenchen <- aggregate(divi_muenchen$faelle_covid_aktuell_invasiv_beatmet, divi_muenchen[1], sum)
 divi_IAA <- aggregate(divi_IAA$faelle_covid_aktuell_invasiv_beatmet, divi_IAA[1], sum)
 divi_IAA$invasiv_anteil <- divi_muenchen$x / divi_IAA$x
-# Fälle IAA Deutschland
-ggplot(data = data_IAA_germany, aes(x = Meldedatum, y = x)) +
-  geom_bar(stat="identity", width = 1) + 
-  labs(x = "Datum", y = "Neu-Infektionen mit Covid-19", title = "Covid-Infektionen um die IAA deutschlandweit") + 
-  annotate("rect", fill = "pink", alpha = 0.4, 
-           xmin = as.Date(c("2021-09-07")) - 0.5 , xmax = as.Date(c("2021-09-12")) + 0.5,
-           ymin = 0, ymax = Inf)  +
-  annotate("rect", fill = "skyblue1", alpha = 0.4, 
-           xmin = as.Date(c("2021-09-14")) -0.5, xmax = as.Date(c("2021-09-19")) +0.5,
-           ymin = 0, ymax = Inf)  +
-  scale_x_date(date_breaks = "1 week", date_labels = "%b %d") + 
-  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
 
-# Fälle IAA Bayern
-IAA_bayern_plot <- ggplot(data = data_IAA_bayern, aes(x = Meldedatum, y = x)) +
-  geom_bar(stat="identity", width = 1) + 
-  labs(x = "Datum", y = "Covid Neu-Infektionen", title = "Covid-Infektionen um die IAA bayernweit") +
-  annotate("rect", fill = "pink", alpha = 0.4, 
-           xmin = as.Date(c("2021-09-07")) - 0.5 , xmax = as.Date(c("2021-09-12")) + 0.5,
-           ymin = 0, ymax = Inf)  +
-  annotate("rect", fill = "skyblue1", alpha = 0.4, 
-           xmin = as.Date(c("2021-09-14")) -0.5, xmax = as.Date(c("2021-09-19")) +0.5,
-           ymin = 0, ymax = Inf)  +
-  scale_x_date(date_breaks = "1 week", date_labels = "%b %d") + 
- theme(axis.text.x = element_text(size = 18, angle = 45, vjust = 1, hjust = 1, face = "bold")) +
-  theme(axis.text.y = element_text(size = 18, face = "bold")) +
- theme(text = element_text(size = 30))
 
 
 # Fälle IAA München
 IAA_muenchen_plot <- ggplot(data = data_IAA_muenchen, aes(x = Meldedatum, y = x / 14.72)) +
   geom_bar(stat="identity", width = 1) + 
-  labs(x = "Datum", y = "Covid Neu-Infektionen\npro 100.000 Einwohner", title = "Covid-Infektionen um die IAA in München") +
+  labs(x = "Datum", y = "Covid-Infektionen\npro 100.000 Einwohner", title = "Tägliche Covid-Infektionen pro 100.000 EInwohner um die IAA in München") +
   annotate("rect", fill = "pink", alpha = 0.4, 
            xmin = as.Date(c("2021-09-07")) - 0.5 , xmax = as.Date(c("2021-09-12")) + 0.5,
            ymin = 0, ymax = Inf)  +
   annotate("rect", fill = "skyblue1", alpha = 0.4, 
            xmin = as.Date(c("2021-09-14")) -0.5, xmax = as.Date(c("2021-09-26")) +0.5,
            ymin = 0, ymax = Inf)  +
-  scale_x_date(date_breaks = "1 week", date_labels = "%b %d") + 
-theme(axis.text.x = element_text(size = 18, angle = 45, vjust = 1, hjust = 1, face = "bold")) +
-  theme(axis.text.y = element_text(size = 18, face = "bold")) +
- theme(text = element_text(size = 30))
+  scale_x_date(date_breaks = "1 week", date_labels = "%d. %b %y") + 
+theme(axis.text.x = element_text(size = 16, angle = 45, vjust = 1, hjust = 1, face = "bold")) +
+  theme(axis.text.y = element_text(size = 16, face = "bold")) +
+ theme(text = element_text(size = 23))
 
 
 # Fälle IAA Bayern ohne München 
 IAA_bayern_ohne_muenchen_plot <- ggplot(data = data_IAA_bayern_ohne_muenchen, aes(x = Meldedatum, y = x / 130.8)) +
   geom_bar(stat="identity", width = 1) + 
-  labs(x = "Datum", y = "Covid Neu-Infektionen\npro 100.000 Einwohner", title = "Covid-Infektionen um die IAA in Rest-Bayern") +
+  labs(x = "Datum", y = "Covid-Infektionen\npro 100.000 Einwohner", title = "Tägliche Covid-Infektionen pro 100.000 Einwohner um die IAA in Rest-Bayern") +
   annotate("rect", fill = "pink", alpha = 0.4, 
            xmin = as.Date(c("2021-09-07")) - 0.5 , xmax = as.Date(c("2021-09-12")) + 0.5,
            ymin = 0, ymax = Inf)  +
   annotate("rect", fill = "skyblue1", alpha = 0.4, 
            xmin = as.Date(c("2021-09-14")) -0.5, xmax = as.Date(c("2021-09-26")) +0.5,
            ymin = 0, ymax = Inf)  +
-  scale_x_date(date_breaks = "1 week", date_labels = "%b %d") + 
-  theme(axis.text.x = element_text(size = 18, angle = 45, vjust = 1, hjust = 1, face = "bold")) +
-  theme(axis.text.y = element_text(size = 18, face = "bold")) +
-  theme(text = element_text(size = 30))
+  scale_x_date(date_breaks = "1 week", date_labels = "%d. %b %y") + 
+  theme(axis.text.x = element_text(size = 16, angle = 45, vjust = 1, hjust = 1, face = "bold")) +
+  theme(axis.text.y = element_text(size = 16, face = "bold")) +
+  theme(text = element_text(size = 23))
 
 
 #Zusammenhängende Grafik
@@ -80,32 +54,32 @@ ggarrange(IAA_muenchen_plot, IAA_bayern_ohne_muenchen_plot, nrow = 2)
 # Fälle IAA Anteil München an Bayern
 ggplot(data = data_IAA_muenchen, aes(y = anteil, x = Meldedatum)) + 
   geom_line(size = 2, color = "black") +
-  labs(x = "Datum", y = "Anteil an Covid-Infektionen", title = "Anteil der münchner Covid-Infektionen an Bayern um die IAA") +
+  labs(x = "Datum", y = "Anteil der Covid-Infektionen", title = "Anteil der münchner Covid-Infektionen an Bayern um die IAA") +
   annotate("rect", fill = "pink", alpha = 0.4, 
            xmin = as.Date(c("2021-09-07")) - 0.5 , xmax = as.Date(c("2021-09-12")) + 0.5,
            ymin = 0, ymax = Inf)  +
   annotate("rect", fill = "skyblue1", alpha = 0.4, 
            xmin = as.Date(c("2021-09-14")) -0.5, xmax = as.Date(c("2021-09-26")) +0.5,
            ymin = 0, ymax = Inf)  +
-  scale_x_date(date_breaks = "1 week", date_labels = "%b %d") + 
+  scale_x_date(date_breaks = "1 week", date_labels = "%d. %b %y") + 
   theme(axis.text.x = element_text(size = 18, angle = 45, vjust = 1, hjust = 1, face = "bold")) +
   theme(axis.text.y = element_text(size = 18, face = "bold")) +
- theme(text = element_text(size = 30))
+ theme(text = element_text(size = 27))
 
 # belegte Intensivbetten Prozent
 ggplot(data = divi_IAA, aes(x = date, y = invasiv_anteil)) +
   geom_line(stat="identity", size = 2) + 
-  labs(x = "Datum", y = "Anteil an invasiv-beatmeten Patienten", title = "Anteil Münchens an invasiv-beatmeten Patienten in Bayern um die IAA") +
+  labs(x = "Datum", y = "Anteil der invasiv-beatmeten\nCovid-Patienten", title = "Anteil der invasiv-beatmeten Covid-Patienten Münchens an Bayern um die IAA") +
   annotate("rect", fill = "pink", alpha = 0.4, 
            xmin = as.Date(c("2021-09-07")) - 0.5, xmax = as.Date(c("2021-09-12")) + 0.5,
            ymin = 0, ymax = Inf)  +
 annotate("rect", fill = "plum4", alpha = 0.4, 
            xmin = as.Date(c("2021-09-21")) - 0.5, xmax = as.Date(c("2021-10-03")) + 0.5,
            ymin = 0, ymax = Inf)  +
-  scale_x_date(date_breaks = "1 week", date_labels = "%b %d") +  
+  scale_x_date(date_breaks = "1 week", date_labels = "%d. %b %y") +  
 theme(axis.text.x = element_text(size = 18, angle = 45, vjust = 1, hjust = 1, face = "bold")) +
   theme(axis.text.y = element_text(size = 18, face = "bold")) +
- theme(text = element_text(size = 30))
+ theme(text = element_text(size = 27))
 
 # datensätze vernichten
 divi_IAA <- NULL
